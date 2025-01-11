b29af032b00de8a86f80a01e138d8c3846453162
mrnarendra9867 <narendrakumark750@gmail.com>
2025-01-11 16:45:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+const Blog = require('./models/blog');
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog")
+
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+const app = express();
+const PORT = 7305;
+
+// Connect to MongoDB
+mongoose
+    .connect("mongodb://127.0.0.1:27017/Blogify", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB!"))
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB:", err.message);
+        process.exit(1); // Exit the application if DB connection fails
+    });
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+// Home Route
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
+
+// This route should render the signup form (GET request)
+app.get("/user/signup", (req, res) => {
+    res.render("signup");
+});
+
+// Routes for user-related functionalities
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+});
